930d3078534eea18aae97282e7ed887cdebe6617
Sofie Nordahl Erner <sofie@erner.dk>
2024-08-06 15:58:52 +0100

    Removed previous attempt at changing between experiences, implemted start of change

--- assets/js/buttonExp.js
+++ assets/js/buttonExp.js
@@ -1,16 +1,22 @@
-function create() {
-    var box = document.createElement("funcTest");
-    box.setAttribute('class', 'itembox')
-    var holder = document.createElement("p");
-    holder.setAttribute('class', 'output');
-    // You can set the inner text of the p tag without creating a text node.
-    holder.innerText = "The text in the box"
-    box.appendChild(holder);
-    // Trades should be an element with and ID because you probably only ever want to insert into one place.
-    var trades = document.getElementById("trades");
-    trades.appendChild(box);
-  }
+const skillbut1 = document.getElementById('skillBut');
+const workbut1 = document.getElementById("workBut");
+const acadbut1 = document.getElementById("acadBut");
 
-  function changeColor() {
-    document.getElementById("myButt").className = "button experience2";
-  }
+const divexp = document.getElementById("expDiv");
+
+function changeToSkills() {
+  divexp.innerHTML = 
+  "<img src='images/Skills_graphic.PNG' alt='Graphics displaying skills with associated levels of experience' width='100%'  /> <p> Code available at <a href='https://github.com/Sofie-Erner' target='_blank' > GitHub </a> </p>";
+}
+skillbut1.addEventListener('click',changeToSkills);
+
+
+function changeToWork() {
+  divexp.innerHTML = "<p> here will be work experiences </p>";
+}
+workbut1.addEventListener('click',changeToWork);
+
+function changeToAcad() {
+  divexp.innerHTML = "<p> here will be academic experiences </p>";
+}
+acadbut1.addEventListener('click',changeToAcad);
