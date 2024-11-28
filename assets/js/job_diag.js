4ab2e599cc86a0ca1ea145f4d06ed3416f048461
Sofie Nordahl Erner <sofie@erner.dk>
2024-11-28 17:30:03 +0100

    added height as parameter to DrawArrow and fixed issue with filling in segments

--- assets/js/job_diag.js
+++ assets/js/job_diag.js
@@ -20,50 +20,91 @@ ctx.font = "bold 20px Lato";
 ctx.textBaseline = "middle";
 ctx.textAlign = "left";
 
-// This script contains the code to draw arrows
+// This function contains the code to draw arrows
 // The arrow will be filled with 4 different segments
-function DrawArrow(x0=0, y0=0, l1=10, l2=10, l3=10, l4=10, c1="#D81B60", c2="#1E88E5", c3="#FFC107", c4="#004D40"){
+function DrawArrow(x0=0, y0=0, yh=100, w1=10, w2=10, w3=10, w4=10, c1="#D81B60", c2="#1E88E5", c3="#FFC107", c4="#004D40"){
     // (x0,y0): starting coordinate from centre of arrow (for reference of placement)
-    // l1-l4: the width of the 4 segments
+    // w1-w4: the width of the 4 segments
     
     // Define variables
-    const l = l1 + l2 + l3 + l4; // total width of arrow
+    const w = w1 + w2 + w3 + w4; // total width of arrow
+    const h = yh - y0;
 
     // Useful x and y coordinates (m for - and p for +)
-    const x1m = x0 - l/2;
-    const x1p = x0 + l/2;
+    const x1m = x0 - w/2;
+    const x1p = x0 + w/2;
 
-    const x2m = x0 - l;
-    const x2p = x0 + l;
+    const x2m = x0 - w;
+    const x2p = x0 + w;
 
-    const y1 = y0 + 3*l/2;
-    const y2 = y0 + 5*l/2;
+    const y1 = y0 + 3*h/4;
+
+    let yTemp; // calculate y-height for segment
+    let xTemp; // calculate x value for segment
+
 
     // Fill 1st segment
     ctx.beginPath();
     ctx.moveTo(x1m,y0);
     ctx.lineTo(x1m,y1);
     ctx.lineTo(x2m,y1);
-    ctx.lineTo(x1m + l1,y2 - l1);
-    ctx.lineTo(x1m + l1,y0);
+    
+    xTemp = x1m + w1;
+
+    if (w1 > w/2){ // if width over half of arrow
+        ctx.lineTo(x0,yh); // draw to middle of arrow
+        yTemp = yh - (h/(4*w))*(w1-w/2);
+    }
+    else {
+        yTemp = y1 + (h/(4*w))*(w/2 + w1);
+    }
+    
+    ctx.lineTo(xTemp,yTemp);
+    ctx.lineTo(xTemp,y0);
     ctx.fillStyle = c1;
     ctx.fill();
 
     // Fill 2nd segment
     ctx.beginPath();
-    ctx.moveTo(x1m + l1,y0);
-    ctx.lineTo(x1m + l1 + l2,y0);
-    ctx.lineTo(x1m + l1 + l2,y2 - l1 + l2);
-    ctx.lineTo(x1m + l1,y2 - l1);
+    ctx.moveTo(xTemp,y0);
+    ctx.lineTo(xTemp,yTemp);
+
+    xTemp = xTemp + w2;
+    if (w1 > w/2){ // if 1st segment width over half of arrow
+        yTemp = yTemp - (h/(4*w))*(w2); // negative gradient
+    }
+    else if( w1 + w2 > w/2){  //if 1st plus 2nd segments width over half of arrow
+        ctx.lineTo(x0,yh); // draw to middle of arrow
+        yTemp = yh - (h/(4*w))*(w2-(w/2-w1));
+    }
+    else { // if still not reached middle
+        yTemp = yTemp + (h/(4*w))*(w2); // positive gradient
+    }
+
+    ctx.lineTo(xTemp,yTemp);
+    ctx.lineTo(xTemp,y0);
     ctx.fillStyle = c2;
     ctx.fill();
 
     // Fill 3nd segment
     ctx.beginPath();
-    ctx.moveTo(x1p - l4,y0);
-    ctx.lineTo(x1p - l4 - l3,y0);
-    ctx.lineTo(x1p - l4 - l3,y2 - l4 + l3);
-    ctx.lineTo(x1p - l4,y2 - l4);
+    ctx.moveTo(xTemp,y0);
+    ctx.lineTo(xTemp,yTemp);
+
+    xTemp = xTemp + w3;
+    if (w1 + w2 > w/2){ // if 1st plus 2nd segment width over half of arrow
+        yTemp = yTemp - (h/(4*w))*(w3); // negative gradient
+    }
+    else if( w1 + w2 + w3 > w/2){  //if the 3 segments width over half of arrow
+        ctx.lineTo(x0,yh); // draw to middle of arrow
+        yTemp = yh - (h/(4*w))*(w3-(w/2-w1-w2));
+    }
+    else { // if still not reached middle
+        yTemp = yTemp + (h/(4*w))*(3); // positive gradient
+    }
+
+    ctx.lineTo(xTemp,yTemp);
+    ctx.lineTo(xTemp,y0);
     ctx.fillStyle = c3;
     ctx.fill();
 
@@ -72,9 +113,20 @@ function DrawArrow(x0=0, y0=0, l1=10, l2=10, l3=10, l4=10, c1="#D81B60", c2="#1E
     ctx.moveTo(x1p,y0);
     ctx.lineTo(x1p,y1);
     ctx.lineTo(x2p,y1);
-    ctx.lineTo(x1p - l4,y2 - l4);
-    ctx.lineTo(x1p - l4,y0);
-    ctx.fillStyle =c4;
+    
+    xTemp = x1p - w4;
+
+    if (w4 > w/2){ // if width over half of arrow
+        ctx.lineTo(x0,yh); // draw to middle of arrow
+        yTemp = yh - (h/(4*w))*(w4-w/2);
+    }
+    else {
+        yTemp = y1 + (h/(4*w))*(w/2 + w4);
+    }
+    
+    ctx.lineTo(xTemp,yTemp);
+    ctx.lineTo(xTemp,y0);
+    ctx.fillStyle = c4;
     ctx.fill();
 
     // Draw the outline arrow
@@ -83,7 +135,7 @@ function DrawArrow(x0=0, y0=0, l1=10, l2=10, l3=10, l4=10, c1="#D81B60", c2="#1E
     ctx.lineTo(x1p,y0);
     ctx.lineTo(x1p,y1);
     ctx.lineTo(x2p,y1);
-    ctx.lineTo(x0,y2);
+    ctx.lineTo(x0,yh);
     ctx.lineTo(x2m,y1);
     ctx.lineTo(x1m,y1);
     ctx.lineTo(x1m,y0);
@@ -93,6 +145,12 @@ function DrawArrow(x0=0, y0=0, l1=10, l2=10, l3=10, l4=10, c1="#D81B60", c2="#1E
     ctx.strokeStyle = "black";
     ctx.stroke();
 };
+
+// Draw segments
+function DrawSeg(){
+
+
+}
 
 // Draw legend at bottom of diagram
 function MakeLegend(canH, c1="#D81B60", c2="#1E88E5", c3="#FFC107", c4="#004D40"){
@@ -136,8 +194,6 @@ function DrawRect(x0=0, y0=0,col = "black",txt,num){
     wRect = textMes.width + 20; // calculate width
     hRect = 2*(textMes.actualBoundingBoxAscent + textMes.actualBoundingBoxDescent) + 40; // calculate height
 
-    console.log(hRect);
-
     ctx.roundRect(x0,y0,wRect,hRect,[10]);
     ctx.fillText(txt,x0 + 10,y0 + 20,wRect,hRect/2);
     ctx.fillText(num,x0 + 10,y0 + 20 + hRect/2,wRect,hRect/2);
@@ -179,4 +235,4 @@ DrawRect(20,(canH-100)/2,"black","Waiting Responses","100");
 wTot = ctx.measureText("Waiting Responses").width + 100;
 DrawRect(wTot,(canH-100)/2,"black","Total","150");
 wTot = ctx.measureText("Waiting Responses").width + 100 + ctx.measureText("Total").width/1.3;
-//DrawArrow(wTot,canH/2+50,38,5,5,2,c1,c2,c3,c4);
+DrawArrow(wTot,canH/2+50,canH/2+150,5,5,5,25,c1,c2,c3,c4);
