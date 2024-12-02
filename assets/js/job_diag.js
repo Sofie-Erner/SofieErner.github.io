57e750bb0babf671a049b376482f5f8ec8663b5b
Sofie Nordahl Erner <sofie@erner.dk>
2024-12-02 15:15:26 +0000

    Implemented layout of boxes and arrows

--- assets/js/job_diag.js
+++ assets/js/job_diag.js
@@ -18,11 +18,14 @@ const canH = canvas.height;
 // Text properties
 ctx.font = "bold 20px Lato";
 ctx.textBaseline = "middle";
-ctx.textAlign = "left";
+ctx.textAlign = "center";
+
+const textMes = ctx.measureText("Testy");
+const hRect = 2*(textMes.actualBoundingBoxAscent + textMes.actualBoundingBoxDescent) + 30; // calculate box height
 
 // This function contains the code to draw arrows
 // The arrow will be filled with 4 different segments
-function DrawArrow(x0=0, y0=0, yh=100, w1=10, w2=10, w3=10, w4=10, c1="#D81B60", c2="#1E88E5", c3="#FFC107", c4="#004D40"){
+function DrawArrow(x0, y0, yh, w1, w2, w3, w4, c1="#D81B60", c2="#1E88E5", c3="#FFC107", c4="#004D40"){
     // (x0,y0): starting coordinate from centre of arrow (for reference of placement)
     // w1-w4: the width of the 4 segments
     
@@ -149,15 +152,18 @@ function DrawArrow(x0=0, y0=0, yh=100, w1=10, w2=10, w3=10, w4=10, c1="#D81B60",
 // Draw legend at bottom of diagram
 function MakeLegend(canH, c1="#D81B60", c2="#1E88E5", c3="#FFC107", c4="#004D40"){
     // Using height of canvas, place legend for colours c1-c4
+    ctx.textAlign = "left";
 
     ctx.strokeStyle = c1;
     ctx.fillStyle = c1;
+
     let txt = "Rejection No Feedback";
     ctx.fillRect(10,canH-30,20,20);
     ctx.fillText(txt,40,canH-20);
 
     ctx.strokeStyle = c2;
     ctx.fillStyle = c2;
+
     let wTxt = ctx.measureText(txt).width;
     txt = "Rejection Not Useful Feedback";
     ctx.fillRect(50+wTxt,canH-30,20,20);
@@ -165,6 +171,7 @@ function MakeLegend(canH, c1="#D81B60", c2="#1E88E5", c3="#FFC107", c4="#004D40"
 
     ctx.strokeStyle = c3;
     ctx.fillStyle = c3;
+
     wTxt = 40 + wTxt + ctx.measureText(txt).width;
     txt = "Rejection Useful Feedback";
     ctx.fillRect(50+wTxt,canH-30,20,20);
@@ -172,35 +179,36 @@ function MakeLegend(canH, c1="#D81B60", c2="#1E88E5", c3="#FFC107", c4="#004D40"
 
     ctx.strokeStyle = c4;
     ctx.fillStyle = c4;
+
     wTxt = 40 + wTxt + ctx.measureText(txt).width;
     txt = "Not Hiring Anymore";
     ctx.fillRect(50+wTxt,canH-30,20,20);
     ctx.fillText(txt,80+wTxt,canH-20);
+
+    ctx.textAlign = "center";
 };
 
 // Draw rounded rectangle with text in it
-function DrawRect(x0=0, y0=0,col = "black",txt,num){
+function DrawRect(x0, y0, w0, h0, col="black", txt, num){
     // place rounded rectangle at (x0,y0) with colour col, text txt, and number num
+    // width and height (w0 and h0) of rectangle
 
     ctx.fillStyle = col;
-    
-    textMes = ctx.measureText(txt);
-    wRect = textMes.width + 20; // calculate width
-    hRect = 2*(textMes.actualBoundingBoxAscent + textMes.actualBoundingBoxDescent) + 40; // calculate height
+    ctx.strokeStyle = col;
 
-    ctx.roundRect(x0,y0,wRect,hRect,[10]);
-    ctx.fillText(txt,x0 + 10,y0 + 20,wRect,hRect/2);
-    ctx.fillText(num,x0 + 10,y0 + 20 + hRect/2,wRect,hRect/2);
+    ctx.roundRect(x0,y0,w0,h0,[10]);
+    ctx.fillText(txt,x0 + w0/2,y0 + 20,w0,h0/2);
+    ctx.fillText(num,x0 + w0/2,y0 + 20 + h0/2,w0,h0/2);
 
     ctx.lineWidth = 2;
     ctx.stroke();
 };
 
 // Draw lines with direction between boxes
-function DrawLine(xS,yS,xF,yF,col = "black"){
+function DrawLine(xS, yS, xF, yF, col="black"){
     // make line with arrow half way to go from one box to another
     // variables
-    const grad1 = (yF-yS)/(xF-yS); // gradient of line
+    //const grad1 = (yF-yS)/(xF-yS); // gradient of line
 
     const alpha = Math.atan(Math.abs(yF - yS)/Math.abs(xF-xS)); // angle of line
     const beta = Math.PI/2 - alpha; // angle for getting arrow points
@@ -216,6 +224,7 @@ function DrawLine(xS,yS,xF,yF,col = "black"){
     const deltaX = Math.cos(beta)*10; // for arrow points
     const deltaY = Math.sin(beta)*10; // for arrow points
 
+    // Draw main line
     ctx.beginPath();
     ctx.moveTo(xS,yS);
     ctx.lineTo(xF,yF);
@@ -223,11 +232,21 @@ function DrawLine(xS,yS,xF,yF,col = "black"){
     ctx.strokeStyle = col;
     ctx.stroke();
 
+    // Draw arrow head
     ctx.beginPath();
-    ctx.moveTo(xB,yB);
-    ctx.lineTo(xB - deltaX, yB + deltaY);
-    ctx.lineTo(xT,yT);
-    ctx.lineTo(xB + deltaX, yB - deltaY);
+    if (xS < xF){ // end after finish point
+        ctx.moveTo(xB,yB);
+        ctx.lineTo(xB - deltaX, yB + deltaY);
+        ctx.lineTo(xT,yT);
+        ctx.lineTo(xB + deltaX, yB - deltaY);
+    }
+    else {
+        ctx.moveTo(xT,yT);
+        ctx.lineTo(xT - deltaX, yT + deltaY);
+        ctx.lineTo(xB,yB);
+        ctx.lineTo(xT + deltaX, yT - deltaY);
+    }
+
     ctx.fillStyle = col;
     ctx.fill();
 };
@@ -236,28 +255,79 @@ function DrawLine(xS,yS,xF,yF,col = "black"){
 // Draw legend
 MakeLegend(canH,c1,c2,c3,c4);
 
+// Text boxes for offers & rejections
+let wRect = 125; // width of box
+DrawRect(canW-wRect-10,10,wRect,hRect,"black","Offers","0");
+DrawRect(canW-wRect-10,canH-hRect-40,wRect,hRect,"black","Rejections","50");
+
 // Make upper and lower lines
 ctx.beginPath();
-ctx.moveTo(10,50);
-ctx.lineTo(canW-200,50);
+ctx.moveTo(10,10 + hRect/2);
+ctx.lineTo(canW-wRect-10,10 + hRect/2);
 
-ctx.moveTo(10,canH-75);
-ctx.lineTo(canW-200,canH-75);
+ctx.moveTo(10,canH - 40 - hRect/2);
+ctx.lineTo(canW-wRect-10,canH - 40 - hRect/2);
 
 ctx.lineWidth = 4;
 ctx.strokeStyle = "black";
 ctx.stroke();
 
-// Text boxes for offers & rejections
-DrawRect(canW-200,15,"black","Offers","0");
-DrawRect(canW-200,canH-115,"black","Rejections","50");
-
 // Waiting responses
-DrawRect(20,(canH-100)/2,"black","Waiting Responses","100");
+let txt = "Waiting Responses";
+wRect = ctx.measureText(txt).width + 20; // update rectangle with
+let hTot = (canH-30-hRect)/2  - 50; // Vertical position
 
+DrawRect(10,hTot, wRect, hRect,"black",txt,"100");
+let wTot = 10 + wRect; // Horizontal position
+
+DrawLine(wTot + 50, hTot + hRect/2, wTot, hTot + hRect/2, col="black");
+wTot = wTot + 50;
 
 // Total Applications
-wTot = ctx.measureText("Waiting Responses").width + 100;
-DrawRect(wTot,(canH-100)/2,"black","Total","150");
-wTot = ctx.measureText("Waiting Responses").width + 100 + ctx.measureText("Total").width/1.3;
-DrawArrow(wTot,canH/2+50,canH/2+150,25,5,5,5,c1,c2,c3,c4);
+txt = "Total";
+wRect = ctx.measureText(txt).width + 20;
+DrawRect(wTot,hTot,wRect,hRect,"black",txt,"160");
+
+DrawArrow(wTot + wRect/2,hTot + hRect, (canH - 40 - hRect/2),25,5,5,5,c1,c2,c3,c4); // arrow for rejections
+wTot = wTot + wRect;
+
+DrawLine(wTot,hTot+ hRect/2, wTot + 53,hTot + hRect + 3, col="black"); // account for rounding of rectangle by adding 3
+wTot = wTot + 50;
+
+// Online Tests
+txt = "Online Test";
+wRect = ctx.measureText(txt).width + 20;
+DrawRect(wTot,hTot + hRect,wRect,hRect,"black",txt,"10");
+
+DrawArrow(wTot + wRect/2,hTot + 2*hRect, (canH - 40 - hRect/2),9,0,0,0,c1,c2,c3,c4); // arrow for rejections
+wTot = wTot + wRect;
+let wTemp = wTot; // temp storage for later use
+
+DrawLine(wTot,hTot + 3*hRect/2, wTot + 53,hTot + 2*hRect + 3, col="black"); // account for rounding of rectangle by adding 3
+wTot = wTot + 50;
+
+// Withdrew Application
+txt = "Withdrew Application";
+wRect = ctx.measureText(txt).width + 20;
+DrawRect(wTot,hTot + 2*hRect,wRect,hRect,"black",txt,"3");
+wTot = wTot + wRect;
+
+// 1st Interview
+txt = "1st Interview";
+wRect = ctx.measureText(txt).width + 20;
+DrawRect(wTot,hTot - hRect,wRect,hRect,"black",txt,"10");
+
+DrawArrow(wTot + wRect/2,hTot, (canH - 40 - hRect/2),9,0,0,0,c1,c2,c3,c4); // arrow for rejections
+wTot = wTot + wRect;
+
+DrawLine(wTemp - 3,hTot + hRect + 3, wTot - wRect,hTot - hRect/2, col="black"); // To Online tests
+DrawLine(wTot - wRect + 3,hTot - 3, wTot - 3*wRect/2,hTot + 2*hRect, col="black"); // To Withdrawn
+DrawLine(wTot,hTot - hRect/2, wTot + 50,hTot - hRect/2, col="black"); // To 2nd Interview
+wTot = wTot + 50;
+
+// 2nd Interview
+txt = "2nd Interview";
+wRect = ctx.measureText(txt).width + 20;
+DrawRect(wTot,hTot - hRect,wRect,hRect,"black",txt,"10");
+
+DrawArrow(wTot + wRect/2,hTot, (canH - 40 - hRect/2),9,0,0,0,c1,c2,c3,c4); // arrow for rejections
